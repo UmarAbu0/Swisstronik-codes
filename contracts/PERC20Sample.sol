17a86060a91174eac3bbf260981a19a6f9c1bd7c
UmarAbu0 abu <uabu0080@gmail.com>
2024-08-06 16:59:02 +0000

    feat: initiated the project

--- contracts/PERC20Sample.sol
+++ contracts/PERC20Sample.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "./PERC20.sol";
 
 contract PERC20Sample is PERC20 {
-    constructor() PERC20("affanu", "aff") {}
+    constructor() PERC20("abu", "ab") {}
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
